afed3fb27422a32c76209ecb8242ba69c129c13b
Kang-Jiho79 <giho2630@gmail.com>
2025-07-28 17:49:00 +0900

    25.07.28
    이름 그리기

--- test.py
+++ test.py
@@ -1,8 +1,78 @@
 import turtle
 
-a = input()
-b = int(a)
-while b!=0:
-    turtle.forward(50)
-    turtle.right(360/int(a))
-    b = b -1
+turtle.forward(100)
+turtle.right(140)
+turtle.forward(100)
+
+turtle.penup()
+turtle.goto(turtle.xcor()+100,turtle.ycor()+115)
+turtle.pendown()
+
+turtle.setheading(-90)
+turtle.forward(75)
+turtle.setheading(0)
+turtle.forward(25)
+turtle.setheading(180)
+turtle.forward(25)
+turtle.setheading(-90)
+turtle.forward(75)
+
+turtle.penup()
+turtle.goto(turtle.xcor()-40,turtle.ycor()-10)
+turtle.pendown()
+
+turtle.setheading(180)
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(turtle.xcor()+150,0)
+turtle.pendown()
+
+turtle.setheading(0)
+turtle.forward(100)
+turtle.right(140)
+turtle.forward(50)
+turtle.setheading(-50)
+turtle.forward(50)
+turtle.left(180)
+turtle.forward(50)
+turtle.setheading(-140)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(turtle.xcor()+100,turtle.ycor()+115)
+turtle.pendown()
+
+turtle.setheading(-90)
+turtle.forward(200)
+
+turtle.penup()
+turtle.goto(turtle.xcor()+100,0)
+turtle.pendown()
+
+turtle.setheading(0)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(20)
+turtle.left(180)
+turtle.forward(20)
+turtle.setheading(0)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(turtle.xcor()-50,turtle.ycor() - 10)
+turtle.pendown()
+
+turtle.setheading(180)
+turtle.circle(30)
+
+turtle.penup()
+turtle.goto(turtle.xcor(),turtle.ycor() - 100)
+turtle.pendown()
+
+turtle.setheading(-90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(180)
+turtle.forward(100)
